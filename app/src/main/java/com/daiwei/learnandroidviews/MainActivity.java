b006ee1dfd909de98023c080eb07d6f0144eebf4
Daiwei Lu <daiwei@fb.com>
2020-05-03 17:02:40 -0700

    list examples of view binding and data bindings

--- app/src/main/java/com/daiwei/learnandroidviews/MainActivity.java
+++ app/src/main/java/com/daiwei/learnandroidviews/MainActivity.java
@@ -2,10 +2,12 @@ package com.daiwei.learnandroidviews;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import com.daiwei.learnandroidviews.databinding.ActivityMainBinding;
 import com.daiwei.learnandroidviews.databinding.DataBindingActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,30 +16,48 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    // Find activity content view
-    // ViewGroup rootView = findViewById(android.R.id.content);
+    setView5();
+  }
 
-    // Option 1: inflate view with XML layout
-    // @SuppressLint("InflateParams") View view = getLayoutInflater().inflate(R.layout.activity_main, null);
+  // Option 1: inflate normal view with XML layout
+  private void setView1() {
+    ViewGroup rootView = findViewById(android.R.id.content);
+    getLayoutInflater().inflate(R.layout.activity_main, rootView);
+  }
 
-    // Option 2: inflate with auto gen view binding class
-    // ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-    // View view = binding.getRoot();
-
-    // Option 3: inflate with auto gen data binding class
-    // DataBindingActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.data_binding_activity_main);
-    // User user = new User("Daiwei", "Lu");
-    // binding.setUser(user);
-
-    DataBindingActivityMainBinding binding = DataBindingActivityMainBinding.inflate(getLayoutInflater());
-    View view = binding.getRoot();
-
-    // Set (display) content
+  // Option 2: inflate normal view with XML layout, can use setContentView() instead of passing root
+  private void setView2() {
+    View view = getLayoutInflater().inflate(R.layout.activity_main, null);
     setContentView(view);
+  }
 
-    binding.setUser(new User("Daiwei", "Lu"));
+  // Option 3: use view binding
+  private void setView3() {
+    ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+    View view = binding.getRoot();
+    setContentView(view);
+  }
 
-    // Use view binding class
-    // binding.displayWords.setText("Daiwei");
+  // Option 4: use data binding + DataBindingUtil
+  private void setView4() {
+    DataBindingActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.data_binding_activity_main);
+    User user = new User("Daiwei", "Lu");
+    binding.setUser(user);
+  }
+
+  // Option 5: use data binding + inflate + setContentView
+  private void setView5() {
+    DataBindingActivityMainBinding binding = DataBindingActivityMainBinding.inflate(getLayoutInflater());
+    User user = new User("Daiwei", "Lu");
+    binding.setUser(user);
+    setContentView(binding.getRoot());
+  }
+
+  // Option 6: use data binding + inflate
+  private void setView6() {
+    ViewGroup rootView = findViewById(android.R.id.content);
+    DataBindingActivityMainBinding binding = DataBindingActivityMainBinding.inflate(getLayoutInflater(), rootView, true);
+    User user = new User("Daiwei", "Lu");
+    binding.setUser(user);
   }
 }
--- app/src/main/java/com/daiwei/learnandroidviews/User.java
+++ app/src/main/java/com/daiwei/learnandroidviews/User.java
@@ -1,18 +1,19 @@
 package com.daiwei.learnandroidviews;
 
 public final class User {
-  public final String firstName;
-  public final String lastName;
+  private final String firstName;
+  private final String lastName;
 
-  public User(String firstName, String lastName) {
+  User(String firstName, String lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
   }
 
   public String getFirstName() {
-      return this.firstName;
+    return this.firstName;
   }
+
   public String getLastName() {
-      return this.lastName;
+    return this.lastName;
   }
 }
